bea34f2ba817c6d847a63263bdbc515db0098dbd
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-12-14 20:46:40 +0300

    level_6 log products

--- level_6/a_log_products.py
+++ level_6/a_log_products.py
@@ -16,27 +16,42 @@ class Product:
         self.price = price
 
     def get_info(self):
-        return f'Product {self.title} with price {self.price}'
+        return f"Product {self.title} with price {self.price}"
 
 
-class PremiumProduct(Product):
+class PrintLoggerMixin:
+    def log(self, message):
+        print(message)
+
+
+class PremiumProduct(Product, PrintLoggerMixin):
     def increase_price(self):
         self.price *= 1.2
+        self.log(f"Price increased to {self.price}")
 
     def get_info(self):
         base_info = super().get_info()
-        return f'{base_info} (Premium)'
+        self.log(f"{base_info} (Premium)")
+        return f"{base_info} (Premium)"
 
 
-class DiscountedProduct(Product):
+class DiscountedProduct(Product, PrintLoggerMixin):
     def decrease_price(self):
         self.price /= 1.2
+        self.log(f"Price decreased to {self.price}")
 
     def get_info(self):
         base_info = super().get_info()
-        return f'{base_info} (Discounted)'
+        self.log(f"{base_info} (Discounted)")
+        return f"{base_info} (Discounted)"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
 
+product1 = PremiumProduct("Product 1", 100)
+product2 = DiscountedProduct("Product 2", 200)
+product1.increase_price()
+product1.get_info()
+product2.decrease_price()
+product2.get_info()
